ed78f79a57dcb2e0d35df8d6ef211d7bcfc7f0f8
UMAtaullin <ataullinural@gmail.com>
2024-09-25 09:30:06 +0500

    add Head & Document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import Head from "next/head";
+import React from "react";
+
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyNext</title>
+        <link key={1} rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>);
 }
 
 export default App;
